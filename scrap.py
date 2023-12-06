f77ebfe4373a7c0666bfac3da1b725797745e7f2
Owais Tahir <oo_wais@hotmail.com>
2023-12-06 15:29:37 +0500

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup as bs
 import logging
 import pandas as pd
 import os
+import sys
 
 city = "Chicago_IL"
 
@@ -10,7 +11,7 @@ url = f"https://www.realtor.com/realestateandhomes-search/{city}"
 
 headers = {
 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
-'Accept-Language': 'en-US,en;q=0.9'
+'Accept-Language': 'en-US;q=1.0,en;q=0.9'
 }
 
 BASE_DIR = os.getcwd()
@@ -18,9 +19,9 @@ logging.basicConfig(filename=os.path.join(BASE_DIR, "scraper_logs_new.log") , le
 
 try:
     response = requests.get(url, headers=headers)
-    soup = bs(response.text)
+    soup = bs(response.text, features='lxml')
     
-    logging.info("Finding all properties...")
+    logging.info(" Finding all properties...")
     all_properties = soup.find_all("div", class_="BasePropertyCard_propertyCardWrap__Z5y4p")
     
     description = []
@@ -34,26 +35,26 @@ try:
     
         if card_content:    
             #extracting description
-            logging.info(f"Collecting description of property {i+1}...")
+            logging.info(f" Collecting description of property {i+1}...")
             description.append(card_content.find("div", class_="base__StyledType-rui__sc-108xfm0-0 kpUjhd message").text)
         
             #extracting prices
-            logging.info(f"Collecting price of property {i+1}...")
+            logging.info(f" Collecting price of property {i+1}...")
             price.append(card_content.find("div", class_="Pricestyles__StyledPrice-rui__btk3ge-0 bvgLFe card-price").text)
         
             #extracting addresses
-            logging.info(f"Collecting address of property {i+1}...")
+            logging.info(f" Collecting address of property {i+1}...")
             address_div = card_content.find("div", class_="card-address truncate-line")
             if address_div:
                 address_parts = address_div.find_all("div", class_="truncate-line")
                 #address = " ".join([part.text.strip() for part in address_parts])
                 address.append(" ".join([part.text.strip() for part in address_parts]))
             else:
-                logging.info("Address not found!!!")
+                logging.warning(" Address not found!!!")
                 address.append("Address not found")
             
             #extracting amount reduced
-            logging.info(f"Collecting reduced amount of property {i+1}...")
+            logging.info(f" Collecting reduced amount of property {i+1}...")
             reduced_amount_div = card_content.find("div", class_="card-reduced-amount")
             if reduced_amount_div:
                 reduced_amount = reduced_amount_div.find("div", class_="Pricestyles__StyledPrice-rui__btk3ge-0 bvgLFe").text
@@ -62,17 +63,19 @@ try:
                 amount_reduced.append("Amount has not reduced")
             
             #extracting links
-            logging.info(f"Collecting web link of property {i+1}...")
+            logging.info(f" Collecting web link of property {i+1}...")
             anchor_tag = card_content.find("a", class_="LinkComponent_anchor__0C2xC")
             if anchor_tag:
                 href = anchor_tag.get("href")
                 link.append(f"https://www.realtor.com/{href}")
             else:
-                logging.info(f"Link not found!!!")
+                logging.warning(f" Link not found!!!")
                 link.append("Link not found")
     
         else:
-            logging.info(f"Card content not found")
+            logging.error(f" Request timed out. Card content not found. Terminating execution!")
+            print("Request timed out. Card content not found. Terminating execution!")
+            sys.exit()
             
         data = {
             "Address": address,
@@ -83,11 +86,11 @@ try:
             }
     
     #creating a pandas dataframe from the extracted data
-    logging.info("Saving extracted data into dataframe...")
+    logging.info(" Saving extracted data into dataframe...")
     df = pd.DataFrame(data)
     
     #saving file as csv
-    logging.info("Saving data as a CSV file...")
+    logging.info(" Saving data as a CSV file...")
     df.to_csv(f"realestate_data_of_{city}.csv")
 
 #catcing exceptions
